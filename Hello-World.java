2dbb40c72a004b66f5ff6717b8ecb07f52f525dd
Lockaa <143623393+Lockaa@users.noreply.github.com>
2026-01-28 13:08:32 -0500

    added print

--- Hello-World.java
+++ Hello-World.java
@@ -1,5 +1,7 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, World\n");
+
+        System.out.println("Goodbye Moonman\n");
     }
 }
